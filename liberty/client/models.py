82eb23823ab627f53537696c96f41b7169476aa5
t.waner <t.waner@gmail.com>
2013-10-08 08:20:10 -0400

    Fixed validation errors

--- liberty/client/models.py
+++ liberty/client/models.py
@@ -1,51 +1,63 @@
 # Client Model
 from django.db import models
-from common.models import Person, Business, Address, Contact
+from common.models import Person
 from employee.models import Employee
 
+
 class BusinessManager(models.Manager):
-	def get_query_set(self):
-		return(super(Client,self).get_query_set().filter(is_business=True))
-		
+    def get_query_set(self):
+        return (super(BusinessManager, self).get_query_set().filter(is_business='True'))
+
+
 class PersonalManager(models.Manager):
-	def get_query_set(self):
-		return(super(Client, self).get_query_set.filter(is_business=False))
+    def get_query_set(self):
+        return (super(PersonalManager, self).get_query_set().filter(is_business='False'))
+
 
 class Client(Person):
-	client_id = models.AutoField(primary_key=True)
-	business_name = models.CharField(max_length=50)
-	is_business = models.BooleanField(default=False)
-	address = models.ForeignKey(Address)
-	contact_info = models.ForeignKey(Contact)
-	billing = models.ForeignKey(Billing_Information)
-	clients = models.Manager()
-	personal = PersonalManager()
-	businesses = BusinessManager()
-	
-	def __unicode__(self)
-		if self.is_business == True:
-			return(self.business_name)
-		else:
-			return(u'%s %s' % (self.first_name, self.last_name))
+    client_id = models.AutoField(primary_key=True)
+    client_number = models.IntegerField(max_length=10)
+    business_name = models.CharField(max_length=50)
+    is_business = models.BooleanField(default=False)
+    address = models.ForeignKey('common.Address')
+    contact_info = models.ForeignKey('common.Contact')
+    billing = models.ForeignKey('common.Billing_Information')
+    clients = models.Manager()
+    personal = PersonalManager()
+    businesses = BusinessManager()
+
+    def __unicode__(self):
+        if self.is_business == True:
+            return (self.business_name)
+        else:
+            return (u'%s %s' % (self.first_name, self.last_name))
+
 
 class Sales_Prospect(Person):
-	sales_prospect_id = models.AutoField(primary_key=True)
-	# TODO - 10/6 - Make this choices
-	liberty_contact = models.ForeignKey(Employee)
-	sale_type = models.CharField(max_length=40)
-	probability = models.CharField(max_length=30)
-	initial_contact_date = models.DateField()
-	comments = models.CharField(max_length=500)
-	
-	def __unicode__(self):
-		return(u'%s %s' % (self.first_name, self.last_name))
-	
+    sales_prospect_id = models.AutoField(primary_key=True)
+    # TODO - 10/6 - Make this choices
+    liberty_contact = models.ForeignKey(Employee)
+    sale_type = models.CharField(max_length=40)
+    probability = models.CharField(max_length=30)
+    initial_contact_date = models.DateField()
+    comments = models.CharField(max_length=500)
+
+    def __unicode__(self):
+        return (u'%s %s' % (self.first_name, self.last_name))
+
+
 class Billing_Information(models.Model):
-	billing_id = models.AutoField(primary_key=True)
-	attention_first_name = models.CharField(max_length=30)
-	attention_last_name = models.CharField(max_length=30)
-	
-		def __unicode__(self):
-			return(self)
+    billing_id = models.AutoField(primary_key=True)
+    attention_first_name = models.CharField(max_length=30)
+    attention_last_name = models.CharField(max_length=30)
+    business_name = models.CharField(max_length=30)
+    is_business = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        if self.is_business == True:
+            return (self.business_name)
+        else:
+            return (u'%s %s' % (self.attention_first_name, self.attention_last_name))
+
 
 
--- liberty/employee/models.py
+++ liberty/employee/models.py
@@ -1,41 +1,42 @@
 #Employee models
 from django.db import models
-from common.models import Person, Address, Contact
+from common.models import Person #, Address, Contact
+
+
+class Title(models.Model):
+    SALES = 'S'
+    TECHNICIAN = 'T'
+    OFFICE = 'O'
+    MARKETING = 'M'
+    CONTRACTOR = 'C'
+    TITLE_CHOICES = (
+        (SALES, "Sales"),
+        (TECHNICIAN, "Technician"),
+        (OFFICE, "Office"),
+        (MARKETING, "Marketing"),
+        (CONTRACTOR, "Contractor"),
+    )
+    title_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=2, choices=TITLE_CHOICES)
+
+    def __unicode__(self):
+        return (self.title)
+
 
 class Employee(Person):
-	employee_id = models.AutoField(primary_key=True)
-	title = models.ManyToManyField(Title)
-	address = models.ForeignKey(Address)
-	contact_info = models.ForeignKey(Contact)
-	hire_date = models.DateField()
-	pay_type = models.CharField(max_length=20)
-	pay_rate = models.CharField(max_length=20)
-	termination_date = models.DateField()
-	termination_reason = models.CharField(max_length=300)
-	
-	def __unicode__(self):
-		return u'%s %s' % (self.first_name, self.last_name)
-		
-	def worker_is(self):
-		return(self.title)
-		
-class Title(models.Model):
-	SALES = 'S'
-	TECHNICIAN = 'T'
-	OFFICE = 'O'
-	MARKETING = 'M'
-	CONTRACTOR = 'C'
-	TITLE_CHOICES = (
-		(SALES, "Sales"),
-		(TECHNICIAN, "Technician"),
-		(OFFICE, "Office"),
-		(MARKETING, "Marketing"),
-		(CONTRACTOR,"Contractor"),
-	)
-	title_id = models.AutoField(primary_key=True)
-	title = models.CharField(max_length=2, choices=TITLE_CHOICES)
-	
-	def __unicode__(self):
-		return self.title
-		
-	
+    employee_id = models.AutoField(primary_key=True)
+    employee_number = models.IntegerField(max_length=10)
+    title = models.ManyToManyField(Title)
+    address = models.ForeignKey('common.Address')
+    contact_info = models.ForeignKey('common.Contact')
+    hire_date = models.DateField()
+    pay_type = models.CharField(max_length=20)
+    pay_rate = models.CharField(max_length=20)
+    termination_date = models.DateField()
+    termination_reason = models.CharField(max_length=300)
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
+    def worker_is(self):
+        return (self.title)
